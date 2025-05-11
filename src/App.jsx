c91ae4c077e36be316ac3af6fb800962b94f7e7c
=dvidnguyen <=qndjxz@gmail.com>
2025-05-11 16:25:15 +0700

    config theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
+import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
+import { Typography } from "@mui/material";
 function App() {
-
   return (
     <>
       <AccessAlarm />
-      <ThreeDRotation/>
+      <ThreeDRotation />
+      <div>Em la dvid nguyen </div>
+      <Typography variant="subtitle2" gutterBottom color="text.secondary">
+        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 <link
   rel="stylesheet"
   href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-/>
-import CssBaseline from '@mui/material/CssBaseline';
+/>;
+import CssBaseline from "@mui/material/CssBaseline";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CssBaseline/>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
