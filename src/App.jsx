ca107397cb83998f3feb2483c7b482c72a0bf5d8
=dvidnguyen <=qndjxz@gmail.com>
2025-05-12 11:32:08 +0700

    add_select_mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,49 @@ import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import { Button } from "@mui/material";
 import { useColorScheme } from "@mui/material/styles";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsSuggestOutlinedIcon from "@mui/icons-material/SettingsSuggestOutlined";
+function SelectMode() {
+  const { mode, setMode } = useColorScheme();
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setMode(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="select-mode-dark-light">Age</InputLabel>
+      <Select
+        labelId="select-mode-dark-light"
+        id="mode-dark-light"
+        value={mode}
+        label="mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: "flex", alignItems: "center",gap:"7px" }}>
+            <LightModeIcon fontSize="small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={{ display: "flex", alignItems: "center",gap:"7px" }}>
+            <DarkModeOutlinedIcon fontSize="small" /> Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={{ display: "flex", alignItems: "center",gap:"7px" }}>
+            <SettingsSuggestOutlinedIcon fontSize="small" /> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
   return (
@@ -18,8 +61,10 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <SelectMode />
+      <hr />
       <ModeToggle />
-      <hr/>
+      <hr />
       <AccessAlarm />
       <ThreeDRotation />
       <div>Em la dvid nguyen </div>
