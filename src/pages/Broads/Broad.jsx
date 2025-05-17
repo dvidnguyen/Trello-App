96bb00b1fd0a470200605ec5e84d6d4666503bb6
=dvidnguyen <=qndjxz@gmail.com>
2025-05-17 11:48:50 +0700

    config_import_absolute_relative

--- src/pages/Broads/Broad.jsx
+++ src/pages/Broads/Broad.jsx
@@ -1,6 +1,6 @@
 // detail
 import Container from "@mui/material/Container";
-import AppBar from "../../components/AppBar";
+import AppBar from "~/components/AppBar";
 import Box from "@mui/material/Box";
 import BroadBar from "./BroadBar/BroadBar";
 import BroadContent from "./BroadContent/BroadContent";
